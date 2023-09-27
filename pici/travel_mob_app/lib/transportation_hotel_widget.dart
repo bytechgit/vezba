17c68ca03dabd18544a8db970a83133e49346508
marijakrsanin <103654600+marijakrsanin@users.noreply.github.com>
2023-09-27 14:52:58 +0200

    Update transportation_hotel_widget.dart

--- pici/travel_mob_app/lib/transportation_hotel_widget.dart
+++ pici/travel_mob_app/lib/transportation_hotel_widget.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:travel_mob_app/models/transportation_model.dart';
 
 class TransportationHotelWidget extends StatefulWidget {
+  final TransportationModel transportations;
   const TransportationHotelWidget({
+    required this.transportations,
     super.key,
   });
 
@@ -12,60 +14,44 @@ class TransportationHotelWidget extends StatefulWidget {
 }
 
 class _TransportationHotelWidgetState extends State<TransportationHotelWidget> {
-  List<TransportationModel> transfortation = [
-    TransportationModel(
-        numbertransfortation: "04", transfortation: Icons.car_crash_rounded),
-    TransportationModel(
-        numbertransfortation: "05", transfortation: Icons.bus_alert),
-    TransportationModel(
-        numbertransfortation: "06", transfortation: Icons.bike_scooter),
-  ];
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ...transfortation.map(
-          (transfrortations) {
-            return Expanded(
-              flex: 30,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  constraints: const BoxConstraints(minWidth: 30),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.blue,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: FittedBox(
-                            //prilagodi velicinu
-                            child: Icon(
-                              transfrortations.transfortation,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 2,
-                        ),
-                        Expanded(
-                            child: FittedBox(
-                                child: Text(
-                                    transfrortations.numbertransfortation))),
-                      ],
+    return Expanded(
+      flex: 30,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          constraints: const BoxConstraints(minWidth: 30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.blue,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: FittedBox(
+                    //prilagodi velicinu
+                    child: Icon(
+                      widget.transportations.transfortation,
+                      color: Colors.black,
                     ),
                   ),
                 ),
-              ),
-            );
-          },
-        )
-      ],
+                const SizedBox(
+                  width: 2,
+                ),
+                Expanded(
+                    child: FittedBox(
+                        child:
+                            Text(widget.transportations.numberTransfortation))),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
